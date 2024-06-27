49ce522fe916bb45d77a28c0e7e6878f6023ea50
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-06-27 11:17:29 +0200

    init

---
+++ 240627how-many-days.php
@@ -0,0 +1,13 @@
+<?php
+
+/*kata-how-many-days
+Kata 41 per l'especialitat fullstackPHP 27-6-24
+
+Crea una funció que calculi i retorni quants dies hi ha entre dues cadenes de text donades:
+
+La cadena de text que representa una data té el format "dd/mm/yyyy"
+La diferència entre dies serà absoluta(no importa l'ordre de les dates)
+Si alguna de les dates donades no respecta el format de data donat, s'ha de mostrar un error.
+Aquest exercici està basat en Retos de Programación de Mouredev. Concretament el #16 de 2022.*/
+
+?>
